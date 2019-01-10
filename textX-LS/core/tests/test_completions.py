686fba3ca5818092d4d9eb1e6ba052615345c562
Daniel Elero <danixeee@gmail.com>
2019-01-10 14:51:43 +0100

    Mess up dummy test to check CI/CD

--- textX-LS/core/tests/test_completions.py
+++ textX-LS/core/tests/test_completions.py
@@ -4,4 +4,4 @@ from textx_ls_core.features.completions import get_completions
 def test_completions():
     '''Dummy test; will be removed'''
     completion_list = get_completions()
-    assert len(completion_list) == 3
+    assert len(completion_list) == 2
