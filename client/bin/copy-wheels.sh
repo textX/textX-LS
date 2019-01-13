b9eb4a7df2e9cfacf95e2aa657b3c89acfd18c42
Daniel Elero <danixeee@gmail.com>
2019-01-13 01:18:20 +0100

    Fix copy-wheels script

--- client/bin/copy-wheels.sh
+++ client/bin/copy-wheels.sh
@@ -6,12 +6,16 @@ client_wheels_dir="$extension_dir/client/wheels/"
 mkdir -p $client_wheels_dir
 
 core_dir="$extension_dir/textX-LS/core/"
+core_dist_dir="$core_dir/dist"
+
 server_dir="$extension_dir/textX-LS/server/"
+server_dist_dir="$server_dir/dist"
+
 
 # Build wheels
 cd $core_dir && python setup.py sdist bdist_wheel > /dev/null 2>&1
 cd $server_dir && python setup.py sdist bdist_wheel > /dev/null 2>&1
 
 # Copy wheels
-cd "$core_dir/dist" && cp *.whl $client_wheels_dir
-cd "$server_dir/dist" && cp *.whl $client_wheels_dir
+cd $core_dist_dir && cp *.whl $client_wheels_dir
+cd $server_dist_dir && cp *.whl $client_wheels_dir
