568afbdd8723deb39c7afeeb80ad3baf2aca9abf
Daniel Elero <danixeee@gmail.com>
2019-01-13 01:28:46 +0100

    Remove slash from copy-wheels script

--- client/bin/copy-wheels.sh
+++ client/bin/copy-wheels.sh
@@ -5,10 +5,10 @@ extension_dir="$(dirname "$(pwd)")"
 client_wheels_dir="$extension_dir/client/wheels/"
 mkdir -p $client_wheels_dir
 
-core_dir="$extension_dir/textX-LS/core/"
+core_dir="$extension_dir/textX-LS/core"
 core_dist_dir="$core_dir/dist"
 
-server_dir="$extension_dir/textX-LS/server/"
+server_dir="$extension_dir/textX-LS/server"
 server_dist_dir="$server_dir/dist"
 
 
