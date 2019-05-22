fadf76d53572080de6eca2db536b30e7707b1f0c
Daniel Elero <danixeee@gmail.com>
2019-05-22 22:48:10 +0200

    Add robot example

---
+++ examples/robot/tx_robot/__init__.py
@@ -0,0 +1,15 @@
+from inspect import getfile
+from os.path import dirname, join
+
+from textx import LanguageDesc, metamodel_from_file
+
+NAME = "Robot"
+PATTERN = '*.rbt'
+DESCRIPTION = 'A language for describing robot behavior...'
+METAMODEL = metamodel_from_file(join(dirname(__file__), 'robot.tx'))
+
+RobotLang = LanguageDesc(
+    name=NAME,
+    pattern=PATTERN,
+    description=DESCRIPTION,
+    metamodel=METAMODEL)
